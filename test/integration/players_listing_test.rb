120d75116171a6c3ae1381ee5c75546ae66de498
Ryan Bagley <ryan.bagley99@gmail.com>
2018-10-15 14:39:57 +0100

    creating a player's list

---
+++ test/integration/players_listing_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class PlayersListingTest < ActionDispatch::IntegrationTest
+  def setup
+    @player = Player.create!(playername: "ryan", email: "ryan@example.com",
+                                                              password: "password", password_confirmation: "password")
+    @player2 = Player.create!(playername: "john", email: "john@example.com",
+                                                              password: "password", password_confirmation: "password")
+  end
+
+  test "should get players listing" do
+    get players_path
+    assert_template 'players/index'
+    assert_select "a[href=?]", player_path(@player), text: @player.playername.capitalize
+    assert_select "a[href=?]", player_path(@player2), text: @player2.playername.capitalize
+  end
+end
